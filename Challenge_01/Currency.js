b5ae4dc02a894b29ff796aec6783346fb4eaa4bc
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-09 21:41:23 +0530

    updated Currency.js

---
+++ Challenge_01/Currency.js
@@ -0,0 +1,22 @@
+/*
+    We just got back from a trip to Asia, specifically China, Japan, and South Korea. When we got back we have some leftover cash:
+
+        🇨🇳 Chinese yuan
+        🇯🇵 Japanese yen
+        🇰🇷 South Korean won
+        Create a currency.js program that starts with the amount you have in yuan, yen, and won and calculates the total in USD.
+
+        Make sure to Google the current exchange rates!
+
+        The output of the program should look like this:
+
+        What do you have left in yuan? 560
+        What do you have left in yen? 2455
+        What do you have left in won? 3280
+
+        105.5275
+        Cha-ching! Now you have the total in USD. 💰
+
+*/
+
+
